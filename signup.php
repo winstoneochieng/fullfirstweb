22a3669e2234695bcfb34bc5bf2094d3f393323d
winstoneochieng <winstone.ochieng@yahoo.com>
2020-02-19 19:12:39 +0300

    win

--- signup.php
+++ signup.php
@@ -1,6 +1,63 @@
 <?php
     require 'header.php';
     require 'config.php';
+
+    $username=$$email=$password1=$password2=$user_type='';
+    $username_err=$$email_err=$password1_err=$password2_err='';
+    //steps
+//1.grab user data from form
+if (isset($_POST['btn_signup'])){
+    //2.clean data
+    if (isset($_POST['username'])){
+        $username = $_POST['username'];
+    }else{
+        $username_err = "Fill this field";
+    }
+    if (isset($_POST['email'])){
+        $email = $_POST['email'];
+    }else{
+        $email_err = "Fill this field";
+    }
+    if (isset($_POST['user-type'])){
+        $email = $_POST['user-type'];
+    }
+    if (isset($_POST['password1'])){
+        $password1 = $_POST['password1'];
+    }else{
+        $password1 = "Fill this field";
+    }
+    if (isset($_POST['password2'])){
+        $password2 = $_POST['password2'];
+    }else{
+        $password2_err = "Fill this field";
+    }
+    //3.check password matched
+    if ($password1 != $password2){
+        $password2_err = 'Password did not match!';
+    }else{
+        //4.check if user exists
+
+        //5.add user into db
+        //5.1 hash password
+        $password1 = md5($password1);
+
+        //5.2 add user
+
+        $sql = "INSERT INTO `users`(`id`, `username`, `email`, `password`, `user_type`) VALUES (NULL,'$username','$email','$password1','$user_type')";
+        if (mysqli_query($conn, $sql)){
+            //6.take user to login page
+            header("location:login.php");
+            exit();
+        }else{
+            echo "Error:".mysqli_error($conn);
+        }
+
+    }
+
+
+
+}
+
 ?>
 <!--Reg form-->
     <div class="container">
@@ -31,10 +88,10 @@
                                  Supplier <input type="radio" name="user-type" value="supplier">
                              </span>
                              <span>
-                                 Customer <input type="radio" name="user-type" value="customer">
+                                 Customer <input type="radio" checked name="user-type" value="customer">
                              </span>
                          </div>
-                         <button class="btn btn-warning btn-block">CREATE ACCOUNT</button>
+                         <button class="btn btn-warning btn-block" name="btn_signup">CREATE ACCOUNT</button>
                      </fieldset>
                  </form>
              </div>
